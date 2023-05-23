519cc73667036648884ffc7c186d1e054775faf7
Jair Chavez Islas <chamaco_03@hotmail.es>
2023-05-23 08:13:34 -0600

    ???

--- views/dashboard.php
+++ views/dashboard.php
@@ -1,11 +1,31 @@
-<?php
-session_start();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-// Verificar si el usuario ha iniciado sesión
-if (!isset($_SESSION['loggedin'])) {
-  header("Location: ../views/login.php");
-  exit();
-}
-// Resto del código de tu página de inicio de sesión (login.php)
-// Aquí puedes mostrar el formulario de inicio de sesión y manejar la autenticación
-?>
+  <!-- including materialize css -->
+  <link rel="stylesheet" href="../public/css/materialize.min.css">
+  <!-- including materialize icons -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <!-- styles.css -->
+  <link rel="stylesheet" href="../public/css/styles.css">
+
+  <title>Document</title>
+</head>
+<body>
+  
+  <nav class="navbar" id="navbar"></nav>
+
+  <div class="main-container">
+    <div class="left-container">yo</div>
+    <div class="rigth-container"></div>
+  </div>
+
+  <!-- app.js -->
+  <script src="../public/js/app.js"></script>
+  <!-- including materialize js -->
+  <script src="../public/js/materialize.min.js"></script>
+</body>
+</html>
