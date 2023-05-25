456ac2b9c4068328e871e9355ee6ebefe032b731
Diego Pérez <diegoperezperez518@gmail.com>
2023-05-25 13:27:30 -0600

    fixed dashboard navbar view & signup validations

--- views/dashboard.php
+++ views/dashboard.php
@@ -1,3 +1,18 @@
+<?php
+session_start();
+
+// Verificar si el usuario ha iniciado sesión
+if (isset($_SESSION['loggedin'])) {
+    // El usuario ha iniciado sesión, redirigir a la página de inicio
+    header("Location: ../views/dashboard.php");
+    exit();
+} else {
+  header("Location: ../views/login.php");
+  exit();
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
