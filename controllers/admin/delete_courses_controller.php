6904dade287327f128ed95cc281212122008eb6d
Jair Chavez Islas <chamaco_03@hotmail.es>
2023-05-30 12:52:08 -0600

    ectructured controllers

--- controllers/admin/delete_courses_controller.php
+++ controllers/admin/delete_courses_controller.php
@@ -13,25 +13,22 @@ $db_password = "";
 $conection = mysqli_connect($host, $username, $db_password, $database);
 
 if (!$conection) {
-    die("Error to connect to the database: " . mysqli_connect_error());
+    die("Error al conectar a la base de datos: " . mysqli_connect_error());
 }
 
-$name = $_DELETE['name'];
-$description = $_POST['description'];
-$date = $_POST['date'];
-$time = $_POST['hour'];
+$id = $_POST['id']; // Supongamos que el ID del registro a eliminar se envía mediante un formulario POST
 
-$query = "INSERT INTO courses (cour_name, cour_description, cour_date, cour_hour) VALUES ('$name','$description','$date','$time')";
+$query = "DELETE FROM courses WHERE id = '$id'";
 
-$result = mysqli_query($conection,$query);
+$result = mysqli_query($conection, $query);
 
 if ($result) {
     $setTimeOut = 0.5;
     $url = '../../views/dashboard.php';
     header("refresh: $setTimeOut; url=$url");
-    echo('Loading...');
+    echo('Eliminando...');
 } else {
-echo "Error al insertar datos: " . mysqli_error($conection);
+    echo "Error al eliminar datos: " . mysqli_error($conection);
 }
 
 mysqli_close($conection);
